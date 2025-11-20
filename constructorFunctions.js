28c9f11719c5a0bc053776e10501439943a7e09c
Papercuts01 <111815913+Papercuts01@users.noreply.github.com>
2025-11-19 20:24:36 -0500

    Add files via upload

---
+++ constructorFunctions.js
@@ -0,0 +1,57 @@
+
+function Assignment(title, dueDate) {
+    this.title = title;
+    this.dueDate = dueDate;
+
+    //print function that outputs the assignment
+    this.printAssignment = function () { 
+        console.log('   Title: ' + this.title + ' | Due Date: ' + this.dueDate);
+    };
+}
+
+//constructor function for Course
+function Course(courseName, instructor, creditHours, assignments) {
+
+    this.courseName = courseName;
+    this.instructor = instructor;
+    this.creditHours = creditHours;
+    this.assignments = assignments;
+
+    //print function that outputs all the course info
+    this.courseInfo = function () { 
+        console.log('Course: ' + this.courseName + 
+                    ' | Instructor: ' + this.instructor + 
+                    ' | Credit Hours: ' + this.creditHours);
+        console.log('Assignments >>>');
+
+        //deploys the printAssignment() function from the Assignment class
+        for (let a of this.assignments)
+            a.printAssignment(); 
+    };
+}
+
+//creates the assignment instances
+let a1 = new Assignment('Project Proposal', 'Jan 15');
+let a2 = new Assignment('Midterm Report', 'Feb 20');
+let a3 = new Assignment('Final Report', 'Mar 30');
+let a4 = new Assignment('Presentation', 'Apr 10');
+
+//creates the course instances, using the pre-initialized assignments
+let c1 = new Course(
+    'Software Engineering',
+    'Dr. Pepper',
+    3,
+    [a1, a2]
+);
+
+let c2 = new Course(
+    'Data Science',
+    'Dr. Evil',
+    6,
+    [a3, a4]
+);
+
+
+//calls the courseInfo() function to print info for the 2 Course instances
+c1.courseInfo();
+c2.courseInfo();
